e6a4646a367599adbaa472a0760e630615273f3f
matteo-rossi90 <matteo.rossi1314@gmail.com>
2025-02-26 13:40:21 +0100

    feat: add wrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Wrapper from './components/Wrapper'
 
 import './App.css'
 
@@ -7,6 +8,9 @@ function App() {
 
   return (
     <>
+      <Wrapper>
+        Ciao
+      </Wrapper>
     </>
   )
 }
